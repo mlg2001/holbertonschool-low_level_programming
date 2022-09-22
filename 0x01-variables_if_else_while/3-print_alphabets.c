9722ea924c99a4d5b62a7748a169c37a34166944
mlg2001 <4945@gmail.com>
2022-09-21 21:47:43 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char d;
+for (d = 'a' ; d <= 'z' ; d++)
+{
+putchar(d);
+}
+for (d = 'A' ; d <= 'Z' ; d++)
+{
+putchar(d);
+}
+putchar('\n');
+return (0);
+}
