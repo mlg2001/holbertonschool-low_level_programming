32949e3df216cfde346a4d29b924e50b20934ac1
mlg2001 <4945@gmail.com>
2022-10-10 15:45:56 -0700

    m

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts_recursion - function that prints a string.
+ * @s:string
+ * Return: none
+ */
+void _puts_recursion(char *s)
+{
+s = "lpa\0"	
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_puts_recursion(s++);
+}
